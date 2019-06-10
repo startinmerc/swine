922987d671f6d8272271d6e53fb342e0c69f90b7
STM <startinmerc@gmail.com>
2019-06-10 11:04:21 +0100

    add pages, routes, packages for routing & rendering

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const express = require("express"),
-		app = express();
+		app = express(),
+		bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
 
 app.get("/", (req,res) => {
 	res.render("landing");
+});
+
+app.get("/about", (req,res) => {
+	res.render("about");
+});
+
+app.get("/products", (req,res) => {
+	res.render("products");
+});
+
+app.get("/blog", (req,res) => {
+	res.render("blog");
 });
 
 app.listen(3000, process.env.IP, () => {
